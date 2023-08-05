dc81d0b8d7dab1bd61c065a52a7c6177d02daf86
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-08-05 16:31:21 -0400

    Add 'last updated' notifier to display when the data was updated

--- src/dom.js
+++ src/dom.js
@@ -1,18 +1,26 @@
 // Dom Manipulation Module
 
+import {format, parseISO} from 'date-fns';
+
 export const dom = (() => {
 	const updateCurrentLocation = (data) => {
 		// Heading
 		const header = document.querySelector('.current-header.weather-current');
-		header.textContent = data.location.name;
+		header.textContent = `${data.location.name}, ${data.location.region}`;
+
+		// Last Updated
+		const time = format(parseISO(data.current.last_updated), 'p');
+		const updated = document.createElement('span');
+		updated.textContent = `Last updated: ${time}`;
+		header.appendChild(updated);
 
 		// Current Temp
 		const currentTemp = document.querySelector('#current-temp');
-		currentTemp.textContent = data.temp_f;
+		currentTemp.textContent = data.current.temp_f + '\u00B0' + 'F';
 
 		// Condition
 		const condition = document.querySelector('#condition.weather-current');
-		condition.textContent = data.condition.text;
+		condition.textContent = data.current.condition.text;
 	};
 
 	return {updateCurrentLocation};
