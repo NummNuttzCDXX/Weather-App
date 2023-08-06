efbc16b167f0620678d0cd575b08e2c62d79c9f6
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-08-05 21:32:51 -0400

    Update 7 day forecast on search button click

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ import {dom} from './dom.js';
 // Show data on Search btn click
 const searchBtn = document.querySelector('button.search');
 searchBtn.addEventListener('click', async function search() {
-	const input = document.querySelector('input#search').value;
+	const input = document.querySelector('input#search');
 
-	const data = await weather.getCurrentLocationData(input);
-	dom.updateCurrentLocation(data);
+	const data = await weather.getForecastData(input.value); // Get data
+	dom.updateCurrentLocation(data); // Display Data
+	dom.updateWeekForecast(data, 'f'); // Display weeks Data
+
+	console.log(data); // TEMP
+
+	input.value = ''; // Clear Input
 });
